f75fc0760400dc4a2f29d2f7f9b33e36615690c4
Ashy <antspa767@gmail.com>
2026-01-03 21:15:51 -0600

    M<WEEWROWER38yg73478

--- app/main.py
+++ app/main.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, base_path)
+
 from app import Application
 
 app = Application()
